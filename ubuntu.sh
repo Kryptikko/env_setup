906eccf4064b33f4684d358931815dfbf9261a33
Kryptikko <velin.br.vangelov@gmail.com>
2015-07-12 07:41:47 -0700

    checking for root

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,6 +4,11 @@ Blue='\e[0;34m'
 Grey='\e[0;30m'
 Clean='\e[0;10m'
 
+if [ "$EUID" -ne 0 ]
+	then echo "Please run as root"
+	exit
+fi
+
 echo -e "${Blue}Installing Ubuntu packages ... ${Clean}"
 # apt-get install build-essential \
 # 				cmake \
