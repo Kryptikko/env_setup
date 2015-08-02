20ad56eba85171684c3f23194182461d97db0325
Kryptikko <velin.br.vangelov@gmail.com>
2015-08-02 21:17:01 +0300

    adding a vim theme

--- ubuntu.sh
+++ ubuntu.sh
@@ -10,22 +10,26 @@ if [ "$EUID" -ne 0 ]
 fi
 
 echo -e "${Blue}Installing Ubuntu packages ... ${Clean}"
-# apt-get install build-essential \
-# 				cmake \
-# 				git \
-# 				vim
+ apt-get install build-essential \
+ 				cmake \
+ 				git \
+ 				vim
 
 echo -e "${Blue}Making CapsLock usefull ... ${Clean}"
-#dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
 
 echo -e "${Blue}Setuping Vim ... ${Clean}"
-#cp ./vimrc $HOME/.vimrc
+cp ./vimrc $HOME/.vimrc
 
 echo -e "${Grey}	Installing Pathogen ${Clean}"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo -e "${Grey}	Fetching Molokai Theme ${Clean}"
+wget https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -P ~/.vim/colors
 #echo -e "${Grey}	 Installing Vim Plugins ${Clean}"
 
 echo -e "${Blue}Setuping Bash ... ${Clean}"
 #cp ./bashrc $HOME/.bashrc
 #source  $HOME/.bashrc
+
+echo -e "${Blue}Setuping Bash ... ${Clean}"
